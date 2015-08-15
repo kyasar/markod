d677cc5c31db3d0808d9c3f0bf3b58edaa522d68
Kadir Yasar <kyasar07@gmail.com>
2015-08-15 22:18:55 +0300

    Adapter of the Scanned product list in Spy screen is customized.

---
+++ app/src/main/java/com/dopamin/markod/adapter/ProductListAdapter.java
@@ -0,0 +1,48 @@
+package com.dopamin.markod.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import com.dopamin.markod.R;
+
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * Created by kadir on 14.08.2015.
+ */
+public class ProductListAdapter extends ArrayAdapter<HashMap<String, String>> {
+    public ProductListAdapter(Context context, List<HashMap<String, String>> products) {
+        super(context, 0, products);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        // Get the data item for this position
+        // Check if an existing view is being reused, otherwise inflate the view
+        HashMap<String, String> product = getItem(position);
+
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.product_list_item , parent, false);
+        }
+
+        convertView.setBackgroundResource(R.drawable.listview_selector);
+
+        // Lookup view for data population
+        TextView tvName = (TextView) convertView.findViewById(R.id.product_name);
+        TextView tvBarcode = (TextView) convertView.findViewById(R.id.product_barcode);
+        TextView tvPrice = (TextView) convertView.findViewById(R.id.product_price);
+
+        // Populate the data into the template view using the data object
+        tvName.setText(product.get("name"));
+        tvBarcode.setText(product.get("price"));
+        tvPrice.setText(product.get("barcode"));
+
+        // Return the completed view to render on screen
+        return convertView;
+    }
+}
