f5419a6705492a788e7e4bc0e770656bc26885b7
Kadir Yasar <kyasar07@gmail.com>
2015-09-16 13:57:04 +0300

    Autokeyboard enabled in entering price for the product.

--- app/src/main/java/com/dopamin/markod/dialog/PriceDialogFragment.java
+++ app/src/main/java/com/dopamin/markod/dialog/PriceDialogFragment.java
@@ -4,9 +4,13 @@ import android.app.Dialog;
 import android.app.AlertDialog.Builder;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -43,6 +47,7 @@ public class PriceDialogFragment extends DialogFragment {
 		view = getActivity().getLayoutInflater().inflate(R.layout.price_piker,
 				null);
 		npMain = (EditText) view.findViewById(R.id.numberMain);
+		npMain.requestFocus();
 		npCent = (EditText) view.findViewById(R.id.numberCent);
 
 		builder.setView(view);
@@ -53,7 +58,7 @@ public class PriceDialogFragment extends DialogFragment {
 		builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
 
 			public void onClick(DialogInterface dialog, int which) {
-				
+
 				// check valid data entered?
 				if (npMain.getText().toString().matches("") || npCent.getText().toString().matches("")) {
 					Toast.makeText(getActivity(), "No valid price entered !!", Toast.LENGTH_SHORT).show();
@@ -68,25 +73,32 @@ public class PriceDialogFragment extends DialogFragment {
 					activity.onUserSelectValue(SpyMarketActivity.PRICE_DIALOG_FRAGMENT_SUCC_CODE, npMain.getText().toString() + "."
 							+ npCent.getText().toString());
 				}
-				
+
 				dialog.dismiss();
 			}
 		});
 
 		builder.setNegativeButton("Cancel",
-			new DialogInterface.OnClickListener() {
+				new DialogInterface.OnClickListener() {
 
-				@Override
-				public void onClick(DialogInterface dialog, int which) {
-					// Do nothing
-					Log.v(MainActivity.TAG,
-							"Price NOT Selected. which: " + which);
-					SpyMarketActivity activity = (SpyMarketActivity) getActivity();
-					activity.onUserSelectValue(0, null);	// product discarded
-					dialog.dismiss();
-				}
-			});
+					@Override
+					public void onClick(DialogInterface dialog, int which) {
+						// Do nothing
+						Log.v(MainActivity.TAG,
+								"Price NOT Selected. which: " + which);
+						SpyMarketActivity activity = (SpyMarketActivity) getActivity();
+						activity.onUserSelectValue(0, null);    // product discarded
+						dialog.dismiss();
+					}
+				});
 
 		return builder.create(); // returns an AlertDialog from a Builder.
+	}
+
+	@Nullable
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		getDialog().getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE);
+		return super.onCreateView(inflater, container, savedInstanceState);
 	}
 }
