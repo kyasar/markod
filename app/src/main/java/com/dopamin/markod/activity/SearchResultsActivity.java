931647fb42123ecae054ce910b97706451277279
Kadir Yasar <kyasar07@gmail.com>
2015-09-23 20:34:50 +0300

    SearchResults return BACK to parent bug fixed.

--- app/src/main/java/com/dopamin/markod/activity/SearchResultsActivity.java
+++ app/src/main/java/com/dopamin/markod/activity/SearchResultsActivity.java
@@ -249,7 +249,8 @@ public class SearchResultsActivity extends AppCompatActivity
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        if (id == R.id.action_settings) {
+        if (id == android.R.id.home) {
+            finish();
             return true;
         }
         return super.onOptionsItemSelected(item);
